936f7fd57e86a6784e6236e4c7b3ace35c5b36e5
Josef Jahn <josef.jahn@gmail.com>
2018-08-24 18:51:39 +0200

    Switched to using SI7021 sensor and internet for external climate data for reliability reasons

--- README.md
+++ README.md
@@ -13,23 +13,31 @@ This is a mains project. Make sure you know what you are doing. Proceed at your 
 
   * 1x ESP32 Dev Board (https://www.espressif.com/en/products/hardware/esp32-devkitc/overview - cheap clones can be found on amazon, banggood, etc.)
   * 1x SainSmart dual-channel relay board (https://www.sainsmart.com/products/2-channel-5v-relay-module)
-  * 2x DHT22 sensor (https://www.adafruit.com/product/385) or similar.
+  * 1x SI7021 sensor (https://learn.sparkfun.com/tutorials/si7021-humidity-and-temperature-sensor-hookup-guide) or similar.
+  * 1x I2C Multiplexer (https://learn.adafruit.com/adafruit-tca9548a-1-to-8-i2c-multiplexer-breakout/wiring-and-test) or similar.
   * 1x Mini power supply (For example https://www.aliexpress.com/item/Free-Shipping-1pcs-lot-HLK-PM01-AC-DC-220V-to-5V-mini-power-supply-module-intelligent/32408565688.html)
   * 1x HD44780 2004 LCD Display Bundle (https://www.amazon.de/AZDelivery-HD44780-Display-Zeichen-Schnittstelle/dp/B07DDGNPX1 - make sure the I2C interface board is included!) 
-  * Some case
+  * A fitting case
   * Mains cabling
   * Two mains voltage wall mount / in-line fans of your choice.
 
 ## Wiring
 
-I am too lazy for a diagram. Basically, you have to solder the I2C board onto the LCD, and connect all the grounds and VCCs. I put the DHT22 on +5V to make sure it gets enough voltage at the end of a long cable.
-Then, connect the LCDs SDA to pin 27 on the ESP32, SCL connects to pin 14. The relay board has two signal pins for the two relay, which connect to pin 5 and 13 on the ESP32. Finally, the data pin of the DHT sensors (second from left) connects to pin 25 and 26 on the ESP32.
+I am too lazy for a diagram. Basically, you have to solder the I2C board onto the LCD, and connect it the grounds and VCC (5V) on the ESP32. 
+Connect the I2C multiplexer to GND and 3.3V (NOT 5V!) as well as the I2C data lines SDA (Pin 27 on ESP32) and SCL (Pin 14 on ESP32).
+The LCDs SDA/SCL is connected to the SD0/SC0 pins on the multiplexer, and the SI7021 sensor connects to SD1/SC1. 
+The relay board has two signal pins for the two channels, which connect to pin 5 and 13 on the ESP32. Finally, connect a momentary pushbutton between ground and Pin 32 on the ESP32.
 That's it! All that's left to do is to wire the mains for the fans through the relay contacts.
 
 ## Libraries used
 
 This project uses LiquidCrystal_I2C and DHTesp, install them via the library manager in the Arduino IDE. Do NOT use the adafruit DHT library, it will only cause you grief.
 
+## Configuration
+Check the top of the source for Wifi variables. Basically there is an optional sending of data to another webserver which you can remove if not needed. External temperature and humidity are fetched from another webserver, the data is expected to be inside a simple text file which is web-readable. I'm using this to fetch data from an outside weather station. This reduces complexity because there's no need to worry about I2C over a long wire, but of course you need Wifi for that. And an external weather sensor. But since there's a multiplexer connected, you could add your own second sensor. However you'll likely still need the multiplexer even if you don't intend to do that, because apparently the LCD library does not play well with other I2C devices on the same bus.
+
 ## Operation
-Everything is automatic. Feel free to adapt the values and/or logic in the code to your preference. The controller will display new values every three seconds, but the fans will change state only once every 1.5 minutes or so. This is to avoid rapidy cycling the fans on and off, and to allow sensor values to settle.
+Everything is automatic. Feel free to adapt the values and/or logic in the code to your preference. The controller will display new values every second, but the fans will change state only once per minute. This is to avoid rapidy cycling the fans on and off, and to allow sensor values to settle. For some reason the LCD may not always initialize on powerup. It will however work after a reset - hence the ugly reset button. If someone figures out why this happens and how to fix it, please let me know.
+
+
 
--- fancontrol.ino
+++ fancontrol.ino
@@ -1,50 +1,125 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <DHTesp.h>
-
-#define DHTPIN_1 25 // The inside sensor is on GPIO 25
-#define DHTPIN_2 26 // The outside sensor is on GPIO 26
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include "SparkFun_Si7021_Breakout_Library.h"
 
 const int RELAYPIN_1 = 5; // The intake fan relay is on GPIO 5
 const int RELAYPIN_2 = 13; // The extractor fan relay is on GPIO 13
+const int RESETPIN = 32; // Reset button
+#define TCAADDR 0x70 // I2C Multiplexer Address
 
-int loopcount;
+const char* ssid     = ""; //Your wifi access data
+const char* password = "";
+
+const char* host = "";  //Enter the hostname for where the backend data files are accessible, see related GIT repository
+const char* weatherhost = "";  //Enter the hostname for where external weather data can be accessed
+
 bool fan_in_active;
 bool fan_out_active;
+unsigned long lastSendTime = 0;
+float t_outside = 0.0;
+float h_outside = 0.0;
 
-DHTesp dht_inside;
-DHTesp dht_outside;
+Weather sensor_inside; 
 LiquidCrystal_I2C lcd(0x3F,20,4); // Set the LCD address to 0x3F for a 20 chars and 4 line display (some displays are on address 0x27 instead)
 
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+  delay(10);
+}
+
+bool readPastHeader(WiFiClient *pClient)
+{
+ bool bIsBlank = true;
+ while(true)
+ {
+   if (pClient->available()) 
+   {
+     char c = pClient->read();
+     if(c=='\r' && bIsBlank)
+     {
+       // throw away the /n
+       c = pClient->read();
+       return true;
+     }
+     if(c=='\n')
+       bIsBlank = true;
+     else if(c!='\r')
+       bIsBlank = false;
+     }
+  }
+}
+
+void wifiReconnect() {
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+}
+
 void setup() {
-  loopcount = 29;
+  delay(100);
   fan_in_active = false;
   fan_out_active = false;
   Serial.begin(115200);
   Serial.print("Starting...");
-  delay(500);
+  delay(100);
   pinMode(RELAYPIN_1, OUTPUT);
   digitalWrite(RELAYPIN_1, HIGH);
   pinMode(RELAYPIN_2, OUTPUT);
   digitalWrite(RELAYPIN_2, HIGH);
+  pinMode(RESETPIN, INPUT_PULLUP);
+  delay(200);
+  tcaselect(0);
+  delay(200);
   lcd.begin(27, 14);// initialize the lcd with SDA and SCL pins 27 and 14 respectively
-  lcd.backlight();
+  lcd.backlight();  
   lcd.setCursor(0,0);
   lcd.print("Fan Control");
+  if (WiFi.status() != WL_CONNECTED) {
+    wifiReconnect();
+  }
   lcd.setCursor(0,1);
   lcd.print("(c) Josef Jahn");
-  dht_inside.setup(DHTPIN_1, DHTesp::DHT22);
-  delay(1100);
-  dht_outside.setup(DHTPIN_2, DHTesp::DHT22);
-  delay(1100);
+  tcaselect(1);
+  sensor_inside.begin();
+  delay(100);
+  tcaselect(0);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Outdoor:");
+
+  lcd.setCursor(13,0);
+  lcd.print("Indoor:");
 }
 
 void loop() {
-  loopcount++;
-  float h_inside = dht_inside.getHumidity();
-  float t_inside = dht_inside.getTemperature();
-  float h_outside = dht_outside.getHumidity();
-  float t_outside = dht_outside.getTemperature();
+  if (digitalRead(RESETPIN) == LOW)
+  { // Check if button has been pressed
+    while (digitalRead(RESETPIN) == LOW) {
+      // Wait for button to be released
+    }
+    ESP.restart();
+  }
+  tcaselect(1);
+  float h_inside = sensor_inside.getRH(); ;
+  float t_inside = sensor_inside.getTemp();
+
+//  tcaselect(2);
+//  float h_outside = sensor_outside.getRH();
+//  float t_outside = sensor_outside.getTemp();
 
   Serial.println("Outdoor temp:");
   Serial.println(t_outside);
@@ -56,28 +131,28 @@ void loop() {
   Serial.println(h_inside);
 
   // display temperature and humidity data
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Outdoor:");
+  tcaselect(0);
   lcd.setCursor(0,2);
-  lcd.print(String(t_outside,1));
+  lcd.print(String(t_outside,2));
   lcd.setCursor(0,3);
-  lcd.print(String(h_outside,1));
+  lcd.print(String(h_outside,2));
 
   lcd.setCursor(5,2);
-  lcd.print((char)223);
+  lcd.print(' ');
   lcd.setCursor(6,2);
+  lcd.print((char)223);
+  lcd.setCursor(7,2);
   lcd.print("C");
   lcd.setCursor(5,3);
-  lcd.print("%");
+  lcd.print(" %");
 
-  lcd.setCursor(13,0);
-  lcd.print("Indoor:");
   lcd.setCursor(12,2);
-  lcd.print(String(t_inside,1));
+  lcd.print(String(t_inside,2));
   lcd.setCursor(12,3);
-  lcd.print(String(h_inside,1));
+  lcd.print(String(h_inside,2));
 
+  lcd.setCursor(17,2);
+  lcd.print(' ');
   lcd.setCursor(18,2);
   lcd.print((char)223);
   lcd.setCursor(19,2);
@@ -85,37 +160,42 @@ void loop() {
   lcd.setCursor(18,3);
   lcd.print("%");
 
-  // Check if any sensors failed
-  if (isnan(h_inside) || isnan(t_inside) || isnan(h_outside) || isnan(t_outside)) {
-    Serial.println("Sensor Error!");
-    lcd.setCursor(0,1);
-    lcd.print("ERROR: SENSOR FAILED");
-    delay(3000);
-    ESP.restart();
-    return;
+  if (!isnan(t_inside)) {
+    // If the inside temperature is higher than the threshold, extract the hot air.
+    if (t_inside > 27.0) {
+      lcd.setCursor(17,1);
+      lcd.print("==>");
+      fan_in_active = true;
+    } else {
+      lcd.setCursor(17,1);
+      lcd.print("   ");
+      fan_in_active = false;
+    }
   }
-
-  // If the inside temperature is higher than the threshold, extract the hot air.
-  if (t_inside > 27.0) {
-    lcd.setCursor(17,1);
-    lcd.print("==>");
-    fan_in_active = true;
-  } else {
-    fan_in_active = false;
+  
+  if (!isnan(t_outside) && !isnan(t_outside) && h_outside != 0.0) {
+    // The intake fan only pulls in fresh air if the humidity outside is lower, and the temperature differential is not too large.
+    if (h_inside > h_outside && t_outside < t_inside + 8.0 && t_outside > 15.0) {
+      lcd.setCursor(0,1);
+      lcd.print("==>");
+      fan_out_active = true;
+    } else {
+      lcd.setCursor(0,1);
+      lcd.print("   ");
+      fan_out_active = false;
+    }
   }
+  
+  unsigned long t = millis();
 
-  // The intake fan only pulls in fresh air if the humidity outside is lower, and the temperature differential is not too large.
-  if (h_inside > h_outside && t_outside < t_inside + 8.0 && t_outside > 15.0) {
-    lcd.setCursor(0,1);
-    lcd.print("==>");
-    fan_out_active = true;
-  } else {
-    fan_out_active = false;
+  //millis() does a rollover
+  if (t < lastSendTime) {
+    lastSendTime = t;
   }
+  
+  if (lastSendTime + 60000 < t || lastSendTime == 0) { 
+    lastSendTime = t;
 
-  // Every 30 loops, toggle the fan if needed. This prevents rapid oscillation.
-  if (loopcount > 30) {
-    loopcount = 0;
     if (fan_in_active) {
       digitalWrite(RELAYPIN_1, LOW);
     } else {
@@ -128,7 +208,77 @@ void loop() {
       digitalWrite(RELAYPIN_2, HIGH);
     }
 
+    if (WiFi.status() != WL_CONNECTED) {
+      wifiReconnect();
+      delay(1000);
+    }
+
+
+    Serial.println("Sending Data");
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        delay(3000);
+        return;
+    }
+
+    // This will send the request to the server
+    client.print(String("GET ") + "/status/climate_backend.php?ti="+String(t_inside,2)+"&to="+String(t_outside,2)+"&hi="+String(h_inside,2)+"&ho="+String(h_outside,2)+" HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            break;
+        }
+    }
+    client.stop();
+   
+    if (!client.connect(weatherhost, httpPort)) {
+        Serial.println("connection failed");
+        delay(3000);
+        return;
+    }
+
+    client.print(String("GET ") + "/lasttemperature.txt" + " HTTP/1.1\r\n" +
+                 "Host: " + weatherhost + "\r\n" +
+                 "Connection: Keep-Alive \r\n\r\n");
+    timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    if (client.available()) {
+        readPastHeader(&client);
+        t_outside = client.readStringUntil('\n').toFloat();
+    }
+
+    client.print(String("GET ") + "/lasthumid.txt" + " HTTP/1.1\r\n" +
+                 "Host: " + weatherhost + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    if (client.available()) {
+        readPastHeader(&client);
+        h_outside = client.readStringUntil('\n').toFloat();
+    }
+    client.stop();
   }
 
-  delay(3000);
+  delay(200);
 }
